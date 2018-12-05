856cfe5146e170a99bfb0b3f908ee1d454b6ff8a
galperins4 <galperins4@gmail.com>
2018-12-04 22:41:46 -0600

    Update aws.py

--- util/aws.py
+++ util/aws.py
@@ -1,2 +1,22 @@
 class AWS:
     def __init__(self):
+        pass
+    
+    def import_config(self):
+        pass
+    
+    
+    def lsBucket(self):
+        pass
+    
+    
+    def cpBucket(self):
+        pass
+    
+    
+    def rotate(self):
+        pass
+    
+    
+    def restore(self):
+        pass
