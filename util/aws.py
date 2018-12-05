eba4ea27931575ed1e18447b0c7605d647aea08e
galperins4 <galperins4@gmail.com>
2018-12-04 22:52:25 -0600

    Update aws.py

--- util/aws.py
+++ util/aws.py
@@ -1,3 +1,4 @@
+from cli import CLI
 import json
 from pathlib import Path
 
@@ -5,7 +6,11 @@ class AWS:
     def __init__(self):
         self.snappy_path = Path().resolve().parent
         self.bucket=self.import_config()
-    
+        self.cli=CLI()
+        
+        self.db = self.cli.get_database()
+        self.cli, self.snapshots = cli.self.get_paths()
+
     
     def import_config(self):
         with open(self.snappy_path / 'config/aws.json') as aws_file:
