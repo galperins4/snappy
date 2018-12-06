e3eb180f1a4e74a2d00ca6ddfb20c8a351c7b78e
galperins4 <galperins4@gmail.com>
2018-12-05 21:47:34 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ sudo apt install unzip
 sudo apt-get install python3-pip
 pip3 install setuptools
 pip3 install awscli --upgrade --user
-pip3 install -r requirements.txt
 cp snappy.sh ~/snappy.sh
 cd ~
 chmod a+x snappy.sh
