2f5a0140b91faf766b93786a4d303bc437eb709e
galperins4 <galperins4@gmail.com>
2018-12-06 18:17:28 -0600

    Update fileops.py

--- util/fileops.py
+++ util/fileops.py
@@ -1,2 +1,16 @@
 class FileOps:
     pass
+
+    def createZip(self,f):
+        os.chdir(self.snapshots)
+        subprocess.run(["zip","-r",f+".zip",f])
+    
+    
+    def unzipZip(self,f):
+        os.chdir(self.snapshots)
+        subprocess.run(["unzip",f])
+    
+    
+    def cleanZip(self,f):
+        os.chdir(self.snapshots)
+        subprocess.run(["rm",f])
