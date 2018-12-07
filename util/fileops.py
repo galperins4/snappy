d938c673a1e3226777fc0f33b4d2c36509408779
galperins4 <galperins4@gmail.com>
2018-12-06 18:19:05 -0600

    Update fileops.py

--- util/fileops.py
+++ util/fileops.py
@@ -1,5 +1,28 @@
 class FileOps:
-    pass
+    def __init__(self):
+        self.cli_path = '/ark-core/packages/core-snapshots-cli'
+        self.snap_path = '/.ark/snapshots/'
+        self.env_path = '/.ark/config'
+        
+        self.db = self.get_database()
+        self.cli, self.snapshots = self.get_paths()
+
+        
+    def get_paths(self):
+        home = str(Path.home())
+        c_path = home+self.cli_path
+        s_path = home+self.snap_path+self.db
+     
+        return c_path, s_path
+
+
+    def get_database(self):
+        home = str(Path.home())
+        env = home+self.env_path
+        with open(env + '/network.json') as network_file:
+            network = json.load(network_file)
+
+        return network['name']
 
     def createZip(self,f):
         os.chdir(self.snapshots)
