e65f0ac5d7746606a97c16b57aaae1f6722ab16b
galperins4 <galperins4@gmail.com>
2018-12-07 21:23:04 -0600

    Update cli.py

--- util/cli.py
+++ util/cli.py
@@ -1,23 +1,24 @@
-import json
+#import json
 import os
-from pathlib import Path
+#from pathlib import Path
 from subprocess import run
 from util.fileops import FileOps
-import sys
+#import sys
 
 
 class CLI:
     def __init__(self):
         self.fileops = FileOps()
         
+        '''
         self.cli_path = '/ark-core/packages/core-snapshots-cli'
         self.snap_path = '/.ark/snapshots/'
         self.env_path = '/.ark/config'
         
         self.db = self.get_database()
         self.cli, self.snapshots = self.get_paths()
-
-    '''    
+        '''
+    '''
     def get_paths(self):
         home = str(Path.home())
         c_path = home+self.cli_path
@@ -33,7 +34,6 @@ class CLI:
             network = json.load(network_file)
 
         return network['name']
-
     '''
 
     def start_proc(self):
@@ -58,7 +58,7 @@ class CLI:
     '''
 
     def view_snap(self):
-        dirlist = self.list_folders()
+        dirlist = self.fileops.list_folders()
         print("Available Snapshots:")
         for i in dirlist:
             print(i)
@@ -73,42 +73,42 @@ class CLI:
     '''
 
     def purge_check(self):
-        dirlist = self.list_folders()
+        dirlist = self.fileops.list_folders()
         if len(dirlist) > 4:
-            l,f = self.get_folders()
+            l,f = self.fileops.get_folders()
             self.delete_snap(f)
 
 
     def create_snap(self):
-        os.chdir(self.cli)
-        run(["yarn","create:"+self.db])
+        os.chdir(self.fileops.cli)
+        run(["yarn","create:"+self.fileops.db])
 
      
     def import_snap(self,s):
-        os.chdir(self.cli)
+        os.chdir(self.fileops.cli)
         self.start_proc()
-        run(["yarn","import:"+self.db,"-b",s,"--truncate"])
+        run(["yarn","import:"+self.fileops.db,"-b",s,"--truncate"])
         self.stop_proc()
 
 
     def verify_snap(self,v):
-        os.chdir(self.cli)
-        run(["yarn","verify:"+self.db,"--blocks",v])
+        os.chdir(self.fileops.cli)
+        run(["yarn","verify:"+self.fileops.db,"--blocks",v])
 
 
     def append_snap(self,c):
-        os.chdir(self.cli)
-        run(["yarn","create:"+self.db,"--blocks",c])
+        os.chdir(self.fileops.cli)
+        run(["yarn","create:"+self.fileops.db,"--blocks",c])
 
 
     def rollback(self,b):
-        os.chdir(self.cli)
+        os.chdir(self.fileops.cli)
         self.start_proc()
-        run(["yarn","rollback:"+self.db,"-b",b])
+        run(["yarn","rollback:"+self.fileops.db,"-b",b])
         self.stop_proc()
      
         #delete snaps with blocks beyond rollback value
-        dirlist = self.list_folders()
+        dirlist = self.fileops.list_folders()
         newlist = [int(i[2:]) for i in dirlist]
         for i in newlist:
             if i > int(b):
@@ -117,7 +117,7 @@ class CLI:
 
     def delete_snap(self,f):
         print("Purging Snapshot",f)
-        os.chdir(self.snapshots)
+        os.chdir(self.fileops.snapshots)
         run(["rm","-rf", f])
 
         
@@ -131,29 +131,29 @@ class CLI:
         
     def menu(self):
         if option=="--view":
-               cli.view_snap()
+               self.view_snap()
         elif option=="--create":
-               cli.create_snap()
-               l,f = cli.get_folders()
-               cli.verify_snap(l)
-               cli.purge_check()
+               self.create_snap()
+               l,f = self.get_folders()
+               self.verify_snap(l)
+               self.purge_check()
         elif option=="--append":
-               l,f = cli.get_folders()
-               cli.append_snap(l)
-               l,f = cli.get_folders()
-               cli.verify_snap(l)
-               cli.purge_check()
+               l,f = self.fileops.get_folders()
+               self.append_snap(l)
+               l,f = self.fileops.get_folders()
+               self.verify_snap(l)
+               self.purge_check()
         elif option=="--import":
-               snap_opt = cli.list_folders()
+               snap_opt = self.fileops.list_folders()
                tmp_menu = {}
                for counter, i in enumerate(snap_opt):
                     tmp_menu[counter+1]=i
                     print(counter+1,"-", i)
                snap_select = int(input("Select one of the options noted above "))
                if snap_select in tmp_menu.keys():
-                    cli.import_snap(tmp_menu[snap_select])
+                    self.import_snap(tmp_menu[snap_select])
                else:
                     print("Something went wrong, please try again")
         elif option=="--rollback":
                block = input("What block would you like to rollback to? ")
-               cli.rollback(block)
+               self.rollback(block)
