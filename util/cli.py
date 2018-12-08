262f1936df152bb7c3057bdc7893bf41ad77e34d
galperins4 <galperins4@gmail.com>
2018-12-07 21:43:01 -0600

    Update cli.py

--- util/cli.py
+++ util/cli.py
@@ -10,31 +10,6 @@ class CLI:
     def __init__(self):
         self.fileops = FileOps()
         
-        '''
-        self.cli_path = '/ark-core/packages/core-snapshots-cli'
-        self.snap_path = '/.ark/snapshots/'
-        self.env_path = '/.ark/config'
-        
-        self.db = self.get_database()
-        self.cli, self.snapshots = self.get_paths()
-        '''
-    '''
-    def get_paths(self):
-        home = str(Path.home())
-        c_path = home+self.cli_path
-        s_path = home+self.snap_path+self.db
-     
-        return c_path, s_path
-    
-
-    def get_database(self):
-        home = str(Path.home())
-        env = home+self.env_path
-        with open(env + '/network.json') as network_file:
-            network = json.load(network_file)
-
-        return network['name']
-    '''
 
     def start_proc(self):
         run(["pm2","stop","ark-core-relay", "ark-core-forger"])
@@ -43,19 +18,6 @@ class CLI:
     def stop_proc(self):
         run(["pm2","start","ark-core-relay","ark-core-forger"])
      
-    '''
-    def list_folders(self):
-        try:
-            folders = [item for item in os.listdir(self.snapshots) if os.path.isdir(os.path.join(self.snapshots, item))]
-        except:
-            print("Oops!!! Looks like no snapshots have been taking. Try --create flag to get started.")
-            quit()
-    
-        if 'rollbackTransactions' in folders:
-            folders.remove('rollbackTransactions')
-        
-        return sorted(folders)
-    '''
 
     def view_snap(self):
         dirlist = self.fileops.list_folders()
@@ -63,14 +25,6 @@ class CLI:
         for i in dirlist:
             print(i)
 
-    '''   
-    def get_folders(self):
-        dirlist = self.list_folders()
-        newlist = [int(i[2:]) for i in dirlist]
-        last = "1-"+ str(max(newlist))
-        first = "1-"+str(min(newlist))
-        return last, first
-    '''
 
     def purge_check(self):
         dirlist = self.fileops.list_folders()
