381dd3514d791f75c1a198d7482de6fa6e2459de
galperins4 <galperins4@gmail.com>
2019-01-09 20:30:37 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ sudo apt install zip
 sudo apt install unzip
 sudo apt-get install python3-pip
 pip3 install setuptools
+pip3 install python-dotenv
 pip3 install awscli --upgrade --user
 pip3 install --upgrade b2
 cp snappy.sh ~/snappy.sh
